454266fbf0da778684f6683ab5ad549b52d8eda7
Karolin Kostial <KKostial@anexia.com>
2026-05-11 15:05:09 +0200

    add public docs for vlan

--- v1/vlan.go
+++ v1/vlan.go
@@ -8,18 +8,21 @@ import (
 	"code.anexia.com/se/ks/go-anx-sdk/paging"
 )
 
+// VlanCreateRequest defines all fields available when creating a new vlan.
 type VlanCreateRequest struct {
 	LocationIdentifier  string `json:"location"`
 	VmProvisioning      bool   `json:"vm_provisioning"`
 	DescriptionCustomer string `json:"description_customer"`
 }
 
+// VlanCreateResponse defines the response for a vlan create request.
 type VlanCreateResponse struct {
 	Identifier          string `json:"identifier"`
 	Name                string `json:"name"`
 	DescriptionCustomer string `json:"description_customer"`
 }
 
+// VlanGetResponse represents a full vlan response.
 type VlanGetResponse struct {
 	Identifier          string                        `json:"identifier"`
 	Name                string                        `json:"name"`
@@ -31,7 +34,7 @@ type VlanGetResponse struct {
 	VmProvisioning      bool                          `json:"vm_provisioning"`
 }
 
-// TODO: extract shared location response model used by multiple endpoints.
+// VlanGetResponseLocationItem represents a location in a full vlan response.
 type VlanGetResponseLocationItem struct {
 	Identifier string `json:"identifier"`
 	Code       string `json:"code"`
@@ -42,23 +45,27 @@ type VlanGetResponseLocationItem struct {
 	CityCode   string `json:"city_code"`
 }
 
+// VlanUpdateRequest defines the possible values that can be updated in a vlan. Nil values are ignored.
 type VlanUpdateRequest struct {
 	VmProvisioning      *bool   `json:"vm_provisioning,omitempty"`
 	DescriptionCustomer *string `json:"description_customer,omitempty"`
 }
 
+// VlanUpdateResponse is the response of a vlan update operation.
 type VlanUpdateResponse struct {
 	Identifier          string `json:"identifier"`
 	Name                string `json:"name"`
 	DescriptionCustomer string `json:"description_customer"`
 }
 
+// VlanListParams defines the available parameters for the vlan list endpoint.
 type VlanListParams struct {
 	Page   int    `url:"page,omitempty"`
 	Limit  int    `url:"limit,omitempty"`
 	Search string `url:"search,omitempty"`
 }
 
+// VlanFilteredParams defines the available parameters for the vlan filter endpoint.
 type VlanFilteredParams struct {
 	Page                   int    `url:"page,omitempty"`
 	Limit                  int    `url:"limit,omitempty"`
@@ -69,52 +76,61 @@ type VlanFilteredParams struct {
 	LocationIdentifier     string `url:"location,omitempty"`
 }
 
+// VlanListItem is an item in vlan list responses.
 type VlanListItem struct {
 	Identifier          string `json:"identifier"`
 	Name                string `json:"name"`
 	DescriptionCustomer string `json:"description_customer"`
 }
 
+// VlansClient is an api client for managing vlans.
 type VlansClient struct {
 	transport *internal.Transport
 }
 
+// NewVlansClient creates a new vlans client.
 func NewVlansClient(transport *internal.Transport) *VlansClient {
 	return &VlansClient{
 		transport: transport,
 	}
 }
 
+// Create creates a new vlan.
 func (v *VlansClient) Create(ctx context.Context, request VlanCreateRequest) (VlanCreateResponse, error) {
 	resp := VlanCreateResponse{}
 	err := v.transport.Post(ctx, "/api/vlan/v1/vlan.json", request, &resp)
 	return resp, mapTransportError(err)
 }
 
+// Get returns a vlan by identifier.
 func (v *VlansClient) Get(ctx context.Context, identifier string) (VlanGetResponse, error) {
 	resp := VlanGetResponse{}
 	err := v.transport.Get(ctx, fmt.Sprintf("api/vlan/v1/vlan.json/%s", identifier), &resp, nil)
 	return resp, mapTransportError(err)
 }
 
+// Update updates a vlan by identifier.
 func (v *VlansClient) Update(ctx context.Context, identifier string, request VlanUpdateRequest) (VlanUpdateResponse, error) {
 	resp := VlanUpdateResponse{}
 	err := v.transport.Put(ctx, fmt.Sprintf("/api/vlan/v1/vlan.json/%s", identifier), request, &resp)
 	return resp, mapTransportError(err)
 }
 
+// List returns a paged list of vlans.
 func (v *VlansClient) List(ctx context.Context, params VlanListParams) (paging.PagedResponse[VlanListItem], error) {
 	resp := internal.RequestWrapper[paging.PagedResponse[VlanListItem]]{}
 	err := v.transport.Get(ctx, "/api/vlan/v1/vlan.json", &resp, params)
 	return resp.Data, mapTransportError(err)
 }
 
+// ListFiltered returns a paged list of vlans filtered by the provided parameters.
 func (v *VlansClient) ListFiltered(ctx context.Context, params VlanFilteredParams) (paging.PagedResponse[VlanListItem], error) {
 	resp := internal.RequestWrapper[paging.PagedResponse[VlanListItem]]{}
 	err := v.transport.Get(ctx, "/api/vlan/v1/vlan/filtered.json", &resp, params)
 	return resp.Data, mapTransportError(err)
 }
 
+// Delete deletes a vlan by identifier.
 func (v *VlansClient) Delete(ctx context.Context, identifier string) error {
 	err := v.transport.Delete(ctx, fmt.Sprintf("/api/vlan/v1/vlan.json/%s", identifier))
 	return mapTransportError(err)
