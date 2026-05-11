1cb514ace37b84c6be6467d9da2a3e61f9279d7d
Karolin Kostial <KKostial@anexia.com>
2026-05-11 16:00:33 +0200

    add docs for location.go

--- v1/location.go
+++ v1/location.go
@@ -7,12 +7,14 @@ import (
 	"code.anexia.com/se/ks/go-anx-sdk/paging"
 )
 
+// LocationListParams defines the available parameters for the location list endpoint.
 type LocationListParams struct {
 	Page   int    `url:"page,omitempty"`
 	Limit  int    `url:"limit,omitempty"`
 	Search string `url:"search,omitempty"`
 }
 
+// LocationListItem is an item in the location list response.
 type LocationListItem struct {
 	Identifier string  `json:"identifier"`
 	Code       string  `json:"code"`
@@ -23,16 +25,19 @@ type LocationListItem struct {
 	Lon        *string `json:"lon"`
 }
 
+// LocationsClient is an api client for managing locations.
 type LocationsClient struct {
 	transport *internal.Transport
 }
 
+// NewLocationsClient creates a new location client.
 func NewLocationsClient(transport *internal.Transport) *LocationsClient {
 	return &LocationsClient{
 		transport,
 	}
 }
 
+// List returns a list of paged locations.
 func (v *LocationsClient) List(ctx context.Context, params LocationListParams) (paging.PagedResponse[LocationListItem], error) {
 	resp := internal.RequestWrapper[paging.PagedResponse[LocationListItem]]{}
 	err := v.transport.Get(ctx, "/api/core/v1/location.json", &resp, params)
