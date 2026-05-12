cdbd6b6f250338409c5aede42d9e24c9c1e67d9f
Karolin Kostial <KKostial@anexia.com>
2026-05-12 08:40:51 +0200

    add docs for internal/utils/ptr/ptr.go

--- internal/utils/ptr/ptr.go
+++ internal/utils/ptr/ptr.go
@@ -1,5 +1,7 @@
 package ptr
 
+// To is a helper function that returns a pointer to the passed in value.
+// It is used in places where it is not possible to take a pointer directly, like from a literal or function return value.
 func To[T any](v T) *T {
 	return &v
 }
