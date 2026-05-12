004c66ce86e0875c855df0d306738fcc2db91274
Karolin Kostial <KKostial@anexia.com>
2026-05-12 08:36:17 +0200

    add docs for v1/client.go

--- v1/client.go
+++ v1/client.go
@@ -2,20 +2,24 @@ package v1
 
 import "code.anexia.com/se/ks/go-anx-sdk/internal"
 
+// Client is an anexia v1 api client.
 type Client struct {
 	transport *internal.Transport
 }
 
+// NewClient creates a new v1 api client.
 func NewClient(transport *internal.Transport) *Client {
 	return &Client{
 		transport: transport,
 	}
 }
 
+// Vlans returns a vlans client.
 func (c *Client) Vlans() *VlansClient {
 	return NewVlansClient(c.transport)
 }
 
+// Locations returns a locations client.
 func (c *Client) Locations() *LocationsClient {
 	return NewLocationsClient(c.transport)
 }
