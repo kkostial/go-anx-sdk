1faa6c74956c5b6e17de46a213930e74882e53fc
Karolin Kostial <KKostial@anexia.com>
2026-05-12 09:08:46 +0200

    add doc for methods in internal/response.go

--- internal/response.go
+++ internal/response.go
@@ -1,5 +1,6 @@
 package internal
 
+// RequestWrapper is a wrapper for some anexia api calls.
 type RequestWrapper[T any] struct {
 	State    string   `json:"state"`
 	Messages []string `json:"messages"`
